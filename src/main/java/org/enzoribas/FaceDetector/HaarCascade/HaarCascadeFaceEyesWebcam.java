f33128e23aea84c3a48b6753ae01bd0129af2312
Enzo Ribas <enzorochaleitedinizribas@gmail.com>
2025-09-28 12:34:29 -0300

    Add Haar Cascade face and hand detection modules
    Introduces new Java classes for face and eye detection using Haar Cascade classifiers with webcam input, as well as hand tracking modules using DNN and Haar Cascade approaches. Includes related resources, images, and batch scripts for compilation and execution.

---
+++ src/main/java/org/enzoribas/FaceDetector/HaarCascade/HaarCascadeFaceEyesWebcam.java
@@ -0,0 +1,54 @@
+package org.enzoribas.FaceDetector.HaarCascade;
+
+import org.bytedeco.opencv.opencv_core.Mat;
+import org.bytedeco.opencv.opencv_core.Rect;
+import org.bytedeco.opencv.opencv_core.RectVector;
+import org.bytedeco.opencv.opencv_core.Scalar;
+import org.bytedeco.opencv.opencv_objdetect.CascadeClassifier;
+import org.bytedeco.opencv.opencv_videoio.VideoCapture;
+import org.enzoribas.Config;
+
+import static org.bytedeco.opencv.global.opencv_highgui.imshow;
+import static org.bytedeco.opencv.global.opencv_highgui.waitKey;
+import static org.bytedeco.opencv.global.opencv_imgproc.LINE_8;
+import static org.bytedeco.opencv.global.opencv_imgproc.rectangle;
+
+public class HaarCascadeFaceEyesWebcam {
+
+    public static void main(String[] args) {
+
+        String projectPath = Config.PROJECT_PATH + "FaceDetector/HaarCascade/"; // Adjust the input image path as needed
+        String inputImagePath = projectPath + "images/" + "input.jpg";
+        String Object = "olhos";
+
+        // Carrega classificador Haar (vem com o OpenCV)
+        CascadeClassifier handTracker = new CascadeClassifier(projectPath + "resources/"+ "haarcascade_eye_tree_eyeglasses.xml");
+
+        VideoCapture capture = new VideoCapture(0);
+        if (!capture.isOpened()) {
+            System.out.println("Não foi possível abrir a webcam!");
+            return;
+        }
+
+        Mat frame = new Mat();
+        RectVector hands = new RectVector();
+
+        while (true) {
+            capture.read(frame);
+            if (frame.empty()) break;
+
+            handTracker.detectMultiScale(frame, hands);
+
+            for (int i = 0; i < hands.size(); i++) {
+                Rect hand = hands.get(i);
+                rectangle(frame, hand, new Scalar(0, 255, 0, 0), 2, LINE_8, 0);
+            }
+
+            System.out.println("Número de " + Object + " detectadas: " + hands.size());
+            imshow("WEBCAM", frame);
+            if (waitKey(1) == 27) break; // ESC para sair
+        }
+
+        capture.release();
+    }
+}
---
+++ src/main/java/org/enzoribas/HandTracker/DNN/HandDetector/readme.md
@@ -0,0 +1,9 @@
+# Project made by
+
+> Dr. Andrew Davison
+Dept. of Computer Engineering
+Prince of Songkla University
+Hat yai, Songkhla 90112, Thailand
+E-mail: ad@fivedots.coe.psu.ac.th
+
+http://fivedots.coe.psu.ac.th/~ad/jg
